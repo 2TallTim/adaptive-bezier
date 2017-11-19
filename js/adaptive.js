152a16ae15dc8e65c00003477c4bbd5d26d6913b
Tim <timh26@gmail.com>
2017-11-19 13:09:56 -0500

    Add convex hull subdivision criterion. Add segments counter.

--- js/adaptive.js
+++ js/adaptive.js
@@ -1,6 +1,6 @@
 
 
-var MAX_DEGREE = 8;
+var MAX_DEGREE = 9;
 var MIN_DEGREE = 3;
 
 var MAX_DEPTH = 8;
@@ -52,7 +52,7 @@ var sumDist = function(ptsList){ //Sum of distances adaptive method
         rej = vecsub(a,q);
         sum+= Math.sqrt(dot(rej,rej));
     }
-    return (sum<BezierRenderer.EPSILON);
+    return (sum<BezierRenderer.DISTANCE_EPSILON);
 }
 
 var uniform = function(ptsList){
@@ -69,6 +69,40 @@ var sumAngles = function(ptsList) {
     return (sum<BezierRenderer.ANGLE_EPSILON);
 }
 
+function orient(a,b,c){
+    //Orientation predicate
+    return val = (b.y - a.y) * (c.x - b.x) - (b.x - a.x) * (c.y - b.y);
+}
+
+var convHull = function(ptsList){
+    //Generate hull with "gift wrap" algorithm.
+
+    hull = [];
+    l = 0;
+    for (var i = 0; i < ptsList.length; i++) {
+        if (ptsList[i].x < ptsList[l].x)
+            l = i;
+    }
+    p = l;
+    do{
+        hull.push(ptsList[p]);
+        q = (p+1)%(ptsList.length);
+        for (var i = 0; i < ptsList.length; i++) {
+            if(orient(ptsList[p],ptsList[i],ptsList[q])<0)
+                q = i;
+        }
+        p = q;
+    }while(p!=l);
+    //Calculate area
+    areasum = 0;
+    n = hull.length
+    for (var i = 0; i < n; i++) {
+        areasum+= (hull[i].x*hull[(i+1)%n].y - hull[(i+1)%n].x*hull[i].y );
+    }
+    return (areasum<BezierRenderer.AREA_EPSILON*2);
+
+}
+
 var BezierRenderer = function(canvas){
     BezierRenderer.canvas = canvas;
     BezierRenderer.degree = 5;
@@ -78,8 +112,9 @@ var BezierRenderer = function(canvas){
     BezierRenderer.animDemoU = false;
     BezierRenderer.animHandle = -1;
     BezierRenderer.alternateColors = false;
-    BezierRenderer.EPSILON = 0.25;
+    BezierRenderer.DISTANCE_EPSILON = 0.25;
     BezierRenderer.ANGLE_EPSILON = 0.1;
+    BezierRenderer.AREA_EPSILON = 0.5;
     BezierRenderer.adaptiveFunc = uniform;
     BezierRenderer.adaptiveFuncName = "uniform";
 
@@ -190,7 +225,10 @@ var BezierRenderer = function(canvas){
             ctx.lineTo(bez_pts[i].x,bez_pts[i].y);
             ctx.stroke();
         }
+
+        $("#segcount").text(""+(bez_pts.length-1)+" segments");
     }
+
 }
 
 
@@ -254,7 +292,14 @@ window.onload = function() {
     
     //Renderer object & GUI
     BezierRenderer(canvas);
+    
     var gui = new dat.GUI();
+
+    var segcount = document.createElement('p');
+    segcount.id = "segcount";
+    canvasWrap.appendChild(segcount);
+
+    //GUI Controls
 
     var degController = gui.add(BezierRenderer, 'degree',MIN_DEGREE,MAX_DEGREE,1);
     var onClickFunc = function(value){
@@ -272,37 +317,39 @@ window.onload = function() {
         BezierRenderer.render();
     };
 
-    gui.add(window,"MAX_DEPTH",3,10,1).onChange(redrawFunc);
+    gui.add(window,"MAX_DEPTH",3,10,1).onChange(redrawFunc).name("Max subdivison depth");
 
-    gui.add(BezierRenderer,"EPSILON",0.001,1).onChange(redrawFunc);
+    gui.add(BezierRenderer,"DISTANCE_EPSILON",0.001,1).onChange(redrawFunc).name("Distance Epsilon");
 
-    gui.add(BezierRenderer,"ANGLE_EPSILON",0.01,0.3).onChange(redrawFunc);
+    gui.add(BezierRenderer,"ANGLE_EPSILON",0.01,0.3).onChange(redrawFunc).name("Angle Epsilon");
 
-    gui.add(BezierRenderer,'alternateColors').onChange(redrawFunc);
+    gui.add(BezierRenderer,"AREA_EPSILON",0.01,1).onChange(redrawFunc).name("Area Epsilon");
+
+    gui.add(BezierRenderer,'alternateColors').onChange(redrawFunc).name("Alternate colors");
 
     gui.add(this, 'randomize');
 
-    gui.add(BezierRenderer, 'adaptiveFuncName', { "Uniform": "uniform", "Sum Of Distances": "sumDist", "Sum of Angles": "sumAngles"} ).onChange(function(value){
+    gui.add(BezierRenderer, 'adaptiveFuncName', { "Uniform": "uniform", "Sum Of Distances": "sumDist", "Sum of Angles": "sumAngles", "Hull Area":"convHull"} ).onChange(function(value){
         if(value=="uniform"){
             BezierRenderer.adaptiveFunc = uniform;
         }else if(value == "sumDist"){
             BezierRenderer.adaptiveFunc = sumDist;
         }else if(value == "sumAngles"){
             BezierRenderer.adaptiveFunc = sumAngles;
+        }else if(value == "convHull"){
+            BezierRenderer.adaptiveFunc = convHull;
         }
         BezierRenderer.render();
 
-    });
+    }).name("Adaptive Criterion");
 
     var dcFolder = gui.addFolder('deCasteljau Visualizer');
 
-    var du = dcFolder.add(BezierRenderer,'demoU',0,1,0.01).listen();
+    dcFolder.add(BezierRenderer,'demoU',0,1,0.01).listen().onChange(redrawFunc).name("Subdivision u");
 
-    du.onChange(redrawFunc);
+    dcFolder.add(BezierRenderer,'showDCsubdiv').onChange(redrawFunc).name("Show deCasteljau");
 
-    dcFolder.add(BezierRenderer,'showDCsubdiv').onChange(redrawFunc);
-
-    dcFolder.add(BezierRenderer,'toggleAnimDemoU');
+    dcFolder.add(BezierRenderer,'toggleAnimDemoU').name("Toggle animation");
     randomize();
     onClickFunc(0); //Redraw when we load the page
 
